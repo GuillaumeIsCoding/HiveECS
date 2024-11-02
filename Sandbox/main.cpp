346788b422765eea650c7534e7d7d0ad9e777e05
GuillaumeIsCoding <115158922+GuillaumeIsCoding@users.noreply.github.com>
2024-11-02 10:14:23 -0400

    Feature Health & Weapon Components

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,7 +1,6 @@
 //
 // Created by guill on 2024-10-25.
 //
-#include "iostream"
 #include "ecs/Ecs.h"
 #include "ecs/Query_Builder.h"
 #include "ecs/system/System.h"
@@ -9,5 +8,55 @@
 
 using namespace hive::ecs;
 
+constexpr int DAG_DAMAGE = 5;
+constexpr int AXE_DAMAGE = 15;
+constexpr int SWORD_DAMAGE = 30;
 
+struct Health final : IComponent {
+    uint8_t hp = 100.0;
+
+    ~Health() override = default;
+
+    [[nodiscard]]
+    std::string toString() const override {
+        return "HP : " + std::to_string(hp);
+    }
+};
+
+struct Weapon : IComponent {
+    uint8_t damage;
+
+    ~Weapon() override = default;
+
+    explicit Weapon(const uint8_t& damage) : damage(damage) {}
+
+    [[nodiscard]]
+    std::string toString() const override {
+        return "Attack : " + std::to_string(damage);
+    }
+};
+
+struct Dag final : Weapon {
+    ~Dag() override = default;
+
+    Dag() : Weapon(DAG_DAMAGE) {}
+};
+
+struct Axe final : Weapon {
+    ~Axe() override = default;
+
+    Axe() : Weapon(AXE_DAMAGE) {}
+};
+
+struct Sword final : Weapon {
+    ~Sword() override = default;
+
+    Sword() : Weapon(SWORD_DAMAGE) {}
+};
+
+
+
+int main() {
+    return 0;
+}
 
